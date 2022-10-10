4934a5d8c4ace24d6f5b2576996327586360c7c2
GabrielReguera <gabrielreguera121@hotmail.com>
2022-10-10 15:30:53 -0300

    Data insert

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentComponent implements OnInit {
 
+  name: string = "Gabriel";
+  age= "20";
+  job= "programmer";
+  Hobbies= ["Play volleibol", " Study JavaScript"];
+  car = {
+    name: "350z",
+    year: "2006"
+  };
+
   constructor() { }
 
   ngOnInit(): void {
