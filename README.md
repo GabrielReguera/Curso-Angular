0a9db13536f54c469c74faf10c9a481ce789527d
GabrielReguera <gabrielreguera121@hotmail.com>
2022-10-11 12:03:28 -0300

    Interfaces

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Angular tabalha com SPA'a (Api separada do front);
 
 Utilizado em aplicações de grande porte;
 
-Sua estrutura é mais bem definida, mais simplivcidade e reaproveitamento de código;
+Sua estrutura é mais bem definida, mais simplicidade e reaproveitamento de código;
 
 Integração de TypeScript;
 
-Componentes divididos em 2 partes, ts, html, css e testes;
+Componentes divididos em partes, ts, html, css e testes;
--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,14 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Turca', type: 'Dog' },
-    { name: 'Tom', type: 'Car' },
-    { name: 'Frida', type: 'Dog' },
-    { name: 'Bob', type: 'Horse' },
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 3 },
+    { name: 'Tom', type: 'Car', age: 6 },
+    { name: 'Frida', type: 'Dog', age: 8 },
+    { name: 'Bob', type: 'Horse', age: 1 },
   ];
+
+  animalsDetails = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal): void {
+    this.animalsDetails = `${animal.name} have ${animal.age} years old`;
+  }
 }
